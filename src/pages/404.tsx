51b89f511b4fe3252b1abdf7228be79ae9d9d7b4
Alexander Samsig <koresamsig@gmail.com>
2023-02-20 21:32:47 +0100

    Add our own 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,58 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import nextI18nConfig from "../../next-i18next.config.mjs"
+import Head from "next/head"
+import { useTranslation } from "next-i18next"
+
+const NotFound = () => {
+  const { t } = useTranslation("common")
+
+  return (
+    <>
+      <Head>
+        <title>{t("title")}</title>
+        <meta name="description" content={t("title")} />
+      </Head>
+      <div className="container">
+        <div style={{ padding: "60px" }}></div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <h1
+            style={{
+              borderRight: "2px solid",
+              paddingRight: "15px",
+            }}
+          >
+            404
+          </h1>
+          <h3
+            style={{
+              paddingLeft: "15px",
+            }}
+          >
+            {t("notFound")}
+          </h3>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default NotFound
+
+export const getStaticProps = async ({ locale }: { locale: string }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(
+        locale ?? "no",
+        ["common"],
+        nextI18nConfig,
+        ["no", "en"]
+      )),
+    },
+  }
+}
