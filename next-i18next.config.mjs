8d6dc96d66f88021c41ba26044dbf7ab6d4e567c
Alexander Samsig <koresamsig@gmail.com>
2023-05-13 14:48:47 +0200

    Disable noisy debug output from react-i18next

--- next-i18next.config.mjs
+++ next-i18next.config.mjs
@@ -2,7 +2,8 @@ import path from "path"
 
 /** @type {import("next-i18next").UserConfig} */
 const config = {
-  debug: process.env.NODE_ENV === "development",
+  // To debug next-i18next, uncomment this line:
+  // debug: process.env.NODE_ENV === "development",
   reloadOnPrerender: process.env.NODE_ENV === "development",
   i18n: {
     locales: ["en", "no"],
