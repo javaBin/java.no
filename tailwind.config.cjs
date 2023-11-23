ce58e91a965f8e3f504e49df0f6300b2c9c54d33
Alexander Samsig <koresamsig@gmail.com>
2023-11-23 22:53:14 +0100

    Enable tailwind, but disable preflight so that it doesn't override bootstrap

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {},
   },
